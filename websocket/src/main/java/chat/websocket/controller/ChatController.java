973a9e718827df2dd70d1d085d8bb1c5e03c896e
nateusse <natyeusse@gmail.com>
2024-12-10 11:03:23 -0500

    completed chat websockets

--- websocket/src/main/java/chat/websocket/controller/ChatController.java
+++ websocket/src/main/java/chat/websocket/controller/ChatController.java
@@ -1,6 +1,6 @@
 package chat.websocket.controller;
 
-import chat.websocket.model.Message;
+import chat.websocket.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
